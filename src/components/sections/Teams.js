5c1d59b151e8fd287cd16393a73d858638b1d267
Ezana <ezanafitsum@gmail.com>
2023-02-23 11:28:18 +0300

    moveed space

--- src/components/sections/Teams.js
+++ src/components/sections/Teams.js
@@ -14,9 +14,9 @@ export default function Teams() {
           </p>
         </div>
         <div className="w-full h-full flex flex-col md:flex-row justify-start items-center snap start ">
-          <div className="h-full md:my-10 w-full  mt-10  md:mt-0  md:w-[40%] flex flex-row items-center justify-center pl-[2rem] ">
-            <div className="h-[20rem] md:h-[27rem] md:pb-20 w-[2rem]  bg-[#57BE94] mr-[1rem]"></div>
-            <p className="h-full text-5xl xl:text-6xl font-medium text-left w-[80%] md:w-[70%] ">
+          <div className="h-full md:my-10 w-full  mt-0  md:mt-0  md:w-[40%] flex flex-row items-center justify-center pl-[2rem] md:pl-[0rem] lg:pl-[2rem] ">
+            <div className="h-[20rem] sm:h-[24rem] md:h-[24rem] lg:h-[20rem] md:pb-20 w-[2rem]  bg-[#57BE94] mr-[1rem]"></div>
+            <p className="h-full text-5xl md:text-6xl xl:text-[4rem] font-medium text-left w-[80%] md:w-[100%] ">
               We would like to push the innovative{" "}
               <span
                 className="text-[#57BE94] inline-block -mr-8 sm:-mr-2"
