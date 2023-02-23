3de72e517e6c21423e227234cd6c550167e63693
Ezana <ezanafitsum@gmail.com>
2023-02-23 08:57:31 +0300

    images

--- src/components/elements/TeamImage.js
+++ src/components/elements/TeamImage.js
@@ -2,22 +2,21 @@ import React, { useState } from "react";
 import { motion as m } from "framer-motion";
 import {FaLinkedinIn} from 'react-icons/fa';
 
-function TeamImage({image,link,name,position,id,show,onShow}) {
+function TeamImage({image,link,name,position}) {
     const [isClicked, setisClicked] = useState(false);
-    const handleShow=()=>onShow&&onShow(id);
   return (
     <m.div
       layout
       transition={spring}
-      onClick={handleShow}
-      className={show ? "item-click relative" : "item relative"}
+      onClick={() => setisClicked(!isClicked)}
+      className={isClicked ? "item-click relative" : "item relative"}
     >
       <div className="bg-black rounded-[1rem] h-full">
         <img src={image} className="opacity-60 object-cover object-cover" />
       </div>
       <div
         className={
-          show
+          isClicked
             ? "absolute flex flex-row justify-between items-center text-white bg-[#131622] w-full pb-6 mt-[-7rem] text-left px-3 rounded-[12px] py-5 "
             : " rotate-[270deg] absolute z-10 translate-y-[-12rem] text-white "
         }
@@ -29,7 +28,7 @@ function TeamImage({image,link,name,position,id,show,onShow}) {
           <p className="text-[30px] font-bold">{name}</p>
           <p className="">{position}</p>
         </div>
-        <div className={show ? "text-2xl text-[#57BE94]" : "hidden"}>
+        <div className={isClicked ? "text-2xl text-[#57BE94]" : "hidden"}>
           <a href={link}><FaLinkedinIn/></a>
         </div>
       </div>
